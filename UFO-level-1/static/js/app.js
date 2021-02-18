138134b30f20e98f63434af038e5c06c46f14147
thallegacy <thallegacy@users.noreply.github.com>
2021-02-17 20:03:07 -0500

    foreach function started with row

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,8 +2,18 @@
 var tableData = data;
 
 // Console.log the data from data.js
-console.log(data);
+// console.log(data);
 
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
+// Loop through the data for each table UFO object
+tableData.forEach((UFOSighting) => {
 
+    // Console.log the data to see if being read by the function
+    console.log(UFOSighting);
+    
+    // Append one table row using "tr" tag for each UFO Sighting object
+    var row = tbody.append("tr");
+});
 
