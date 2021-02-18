cdf297d238f81ff66a303db32234d07c47a6ab9b
thallegacy <thallegacy@users.noreply.github.com>
2021-02-17 22:17:10 -0500

    start of event handling

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,27 +1,18 @@
 // from data.js
 var tableData = data;
 
-// Console.log the data from data.js
-// console.log(data);
-
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
 // Loop through the data for each table UFO object
 tableData.forEach((UFOSighting) => {
 
-    // Console.log the data to see if being read by the function
-    // console.log(UFOSighting);
-
     // Append one table row using "tr" tag for each UFO Sighting object
     var row = tbody.append("tr");
     
     // Loop for appending the values for each newly created row
     Object.entries(UFOSighting).forEach(([key, value]) => {
-        
-        // Console.log the data to see if key and values are being read by the function
-        //console.log(key, value);
-
+    
         // Append a cell to the row for each value using "td" tag
         var cell = row.append("td");
          // Append the text from each value to the row
@@ -29,3 +20,12 @@ tableData.forEach((UFOSighting) => {
     });
 });
 
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", dateFilter);
+form.on("submit",runEdateFilternter);
