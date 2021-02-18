1ecb4f796dd01952099e5668b0d0457e3431a7ec
thallegacy <thallegacy@users.noreply.github.com>
2021-02-17 20:15:23 -0500

    foreach loop for appending value

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,9 +11,16 @@ var tbody = d3.select("tbody");
 tableData.forEach((UFOSighting) => {
 
     // Console.log the data to see if being read by the function
-    console.log(UFOSighting);
-    
+    // console.log(UFOSighting);
+
     // Append one table row using "tr" tag for each UFO Sighting object
     var row = tbody.append("tr");
+    
+    // Loop for appending the values for each newly created row
+    Object.entries(UFOSighting).forEach(([key, value]) => {
+        
+        // Console.log the data to see if key and values are being read by the function
+        console.log(key, value);
+    });
 });
 
